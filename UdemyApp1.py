d43501fb8f8c52a140e831834bff5ec1478fb8a6
Soumil <soumilsingh6@gmail.com>
2024-12-24 20:53:23 -0500

    Add + Implement Exit and Complete buttons

--- UdemyApp1.py
+++ UdemyApp1.py
@@ -1,43 +1,51 @@
-import PySimpleGUI as SG
+import PySimpleGUI as sg
 import VisualFunctions as f
 
-label = SG.Text("Type in a todo")
-input_box = SG.InputText(tooltip="Enter todo", key="todo")
-b_ADD = SG.Button("Add")
-box = SG.Listbox(values=f.get_items, key='todos',
+label = sg.Text("Type in a todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+b_ADD = sg.Button("Add")
+lis_box = sg.Listbox(values=f.get_items(), key='todos',
                  enable_events=True, size=[45, 10])
-b_EDIT = SG.Button("Edit")
+b_EDIT = sg.Button("Edit")
+b_COMPLETE = sg.Button("Complete")
+b_EXIT = sg.Button("Exit")
 
-layout = [[label], [input_box, b_ADD], [box, b_EDIT]]
+layout = [[label], [input_box, b_ADD], [lis_box, b_EDIT, b_COMPLETE], [b_EXIT]]
 
-window = SG.Window('My To-Do App', layout=layout, font=("Roboto", 20))
+window = sg.Window('My To-Do App', layout=layout, font=("Roboto", 20))
 while True:
     event, values = window.read()
     print(f'{event}\n{values}')
     match event:
         case 'Add':
             todos = f.get_items()
-            new = values["todo"] + '\n'
+            new = values["todo"]
             todos.append(new)
             f.write_items(todos)
             window['todos'].update(values=todos)
         case 'Edit':
             to_edit = values['todos'][0]
-            new_todo = values['todo'] + '\n'
+            new_todo = values['todo']
 
-            todos = f.get_items()
-            index = todos.index(to_edit)
-            todos[index] = new_todo
-            f.write_items(todos)
-            window['todos'].update(values=todos)
+            tasks = f.get_items()
+            to_edit_index = tasks.index(to_edit)
+            tasks[to_edit_index] = new_todo
+            f.write_items(tasks)
+            window['todos'].update(values=tasks) #window['todos'] is
+            # the listbox that all items are stored in,
+            # update will change it on the spot instead of having to refresh the program
+        case 'Complete':
+            to_complete = values[todos][0]
+            tasks = f.get_items()
+            tasks.remove(to_complete)
+            f.write_items(tasks)
+            window['todos'].update(values=tasks)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
-            print(values)
-            if values['todos'] != '':
                 window['todo'].update(value=values['todos'][0])
-            else:
-                print('Please add somthing to edit!')
-                continue
-        case SG.WIN_CLOSED:
+        case sg.WIN_CLOSED:
             break
 
 window.close()
