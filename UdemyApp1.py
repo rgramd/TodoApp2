4f9fb9140a49e4c4a59991cb294c17f4f0f124bb
Soumil <soumilsingh6@gmail.com>
2025-01-01 16:33:21 -0500

    Change theme, add date/time, fix edit and complete errors

--- UdemyApp1.py
+++ UdemyApp1.py
@@ -1,6 +1,10 @@
 import PySimpleGUI as sg
 import VisualFunctions as f
+import time
 
+sg.theme("DarkRed")
+
+clock = sg.Text('', key='time_now')
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 b_ADD = sg.Button("Add")
@@ -10,42 +14,56 @@ b_EDIT = sg.Button("Edit")
 b_COMPLETE = sg.Button("Complete")
 b_EXIT = sg.Button("Exit")
 
-layout = [[label], [input_box, b_ADD], [lis_box, b_EDIT, b_COMPLETE], [b_EXIT]]
+layout = [[clock],
+          [label],
+          [input_box, b_ADD],
+          [lis_box, b_EDIT, b_COMPLETE],
+          [b_EXIT]]
+# Dimensional arrays will separate code line by line, even if you actually put code on same line
 
 window = sg.Window('My To-Do App', layout=layout, font=("Roboto", 20))
 while True:
-    event, values = window.read()
-    print(f'{event}\n{values}')
+    event, values = window.read(timeout=200)
+    if event != sg.WIN_CLOSED:
+        window['time_now'].update(time.strftime('%b %d %Y %H:%M:%S'))
     match event:
         case 'Add':
             todos = f.get_items()
             new = values["todo"]
-            todos.append(new)
+            todos.append(new + '\n')
             f.write_items(todos)
             window['todos'].update(values=todos)
         case 'Edit':
-            to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            tasks = f.get_items()
-            to_edit_index = tasks.index(to_edit)
-            tasks[to_edit_index] = new_todo
-            f.write_items(tasks)
-            window['todos'].update(values=tasks) #window['todos'] is
-            # the listbox that all items are stored in,
-            # update will change it on the spot instead of having to refresh the program
+                tasks = f.get_items()
+                print(tasks)
+                to_edit_index = tasks.index(to_edit)
+                tasks[to_edit_index] = new_todo
+                f.write_items(tasks)
+                window['todos'].update(values=tasks) #window['todos'] is
+                # the listbox that all items are stored in,
+                # update will change it on the spot instead of having to refresh the program
+            except IndexError:
+                sg.popup("Please select an item to edit!", font=("Helvetica", 20))
         case 'Complete':
-            to_complete = values[todos][0]
-            tasks = f.get_items()
-            tasks.remove(to_complete)
-            f.write_items(tasks)
-            window['todos'].update(values=tasks)
-            window['todo'].update(value='')
+            try:
+                to_complete = values['todos'][0]
+                tasks = f.get_items()
+                tasks.remove(to_complete)
+                f.write_items(tasks)
+                window['todos'].update(values=tasks)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item to complete!", font=("Helvetica", 20))
         case "Exit":
             break
         case 'todos':
-                window['todo'].update(value=values['todos'][0])
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
+
 
 window.close()
